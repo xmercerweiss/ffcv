d91c8799cc55cdcc67ea27c68549a50502996265
Xavier Mercerweiss <mercerweissx@gmail.com>
2024-12-24 19:21:14 -0700

    Began writing source

---
+++ src/main/java/app/Launcher.java
@@ -0,0 +1,12 @@
+package app;
+
+import java.util.Arrays;
+
+
+public class Launcher {
+    
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
+    }
+}
+
